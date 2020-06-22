a2237b4e9b24f727a54b27665ed6010bdbf22691
Radik <radik93@mail.ru>
2020-06-22 11:10:43 +0500

    version 1.0

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        publicPath: 'dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("main.css"),
+    ]
+}
